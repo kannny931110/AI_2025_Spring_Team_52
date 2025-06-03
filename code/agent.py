3f507b7fa1cf27b5261eb4ab6e09bac58ebf7e77
kannny931110 <167603062+kannny931110@users.noreply.github.com>
2025-06-04 02:58:09 +0800

    Update agent.py

--- code/agent.py
+++ code/agent.py
@@ -14,14 +14,14 @@ def retrieve_agent(agent_type: str, agent_id: int, **kwargs):
             agent_id,
             time_limit=kwargs.get("time_limit", 2.0),
             max_depth=kwargs.get("max_depth", 2),
-            weight_mobility=kwargs.get("weight_mobility", 1.0),
-            weight_territory=kwargs.get("weight_territory", 1.0)
+            weight_mobility=kwargs.get("weight_mobility", 0.4),
+            weight_territory=kwargs.get("weight_territory", 0.4)
         )
 
     elif agent_type == "mcts":
         return MCTSAgent(
             agent_id,
-            time_limit=kwargs.get("time_limit", 1.0),
+            time_limit=kwargs.get("time_limit", 2.0),
         )
 
     elif agent_type == "iterative":
